b316cdf6d68521a66ddb24d3d0dd25bdf3fd51a3
Philip Schatz <253202+philschatz@users.noreply.github.com>
2020-03-17 23:49:31 -0500

    build jar

--- com.philschatz.xslt/src/main/java/com/philschatz/xslt/App.java
+++ com.philschatz.xslt/src/main/java/com/philschatz/xslt/App.java
@@ -2,7 +2,12 @@ package com.philschatz.xslt;
 
 public class App {
     public static void main(String[] args) {
-        XSLTDebugServer server = new XSLTDebugServer(8080);
+        if (args.length != 1) {
+            System.err.println("Exactly 1 commandline argument should be provided, the port number to listen on");
+            System.exit(110);
+        }
+        int port = Integer.parseInt(args[0]);
+        XSLTDebugServer server = new XSLTDebugServer(port);
         server.start();
     }
 }
